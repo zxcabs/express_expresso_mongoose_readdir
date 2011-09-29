02d35e6c541e007bba3a9d760d61ebe7ba89e2c2
Evgeny Reznichenko <kusakyky@gmail.com>
2011-09-29 18:50:01 +0400

    fix end str

--- test/index.js
+++ test/index.js
@@ -1,1 +1,16 @@
-var assert = require('assert'),	app = require('../index.js');module.exports = {	'test': function () {		assert.response(app, 			{				method: 'GET',				url: '/404',			}, 			{				status: 200,				body: 'NotFound'			});					}	};
+var assert = require('assert'),
+	app = require('../index.js');
+
+module.exports = {
+	'test': function () {
+		assert.response(app, 
+			{
+				method: 'GET',
+				url: '/404',
+			}, 
+			{
+				status: 200,
+				body: 'NotFound'
+			});				
+	}	
+};
